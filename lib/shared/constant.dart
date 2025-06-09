dce0a7614192f23368e527b26e910a1be43dd982
Thor Yu Guan <luckboy5566@gmail.com>
2025-06-09 14:10:42 +0800

    Update API_DOMAIN in constant.dart; add settings.json for VSCode; bump version to 1.0.1+2 in pubspec.yaml

--- lib/shared/constant.dart
+++ lib/shared/constant.dart
@@ -3,8 +3,8 @@
 
 class Constant {
   // static const String API_DOMAIN = 'localhost:8000';
-  static const String API_DOMAIN = '10.0.2.2:8000'; // for android
-  // static const String API_DOMAIN = 'dev.kanesan.my';
+  // static const String API_DOMAIN = '10.0.2.2:8000'; // for android
+  static const String API_DOMAIN = 'dev.kanesan.my';
 
   static const String ANDROID_ID = 'com.example.bht';
   static const String IOS_ID = 'com.example.bht';
